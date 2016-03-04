65bbda887b4c2fa477f8780ebd0b204360cdd3e9
Soenke Petersen <soen.petersen@gmail.com>
2016-03-04 10:12:30 +0100

    view and edit handler with html templates

--- wiki.go
+++ wiki.go
@@ -4,7 +4,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
@@ -31,15 +31,32 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+//Render a html template
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
+}
+
 //Handler: View a wiki page
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+    renderTemplate(w, "view", p)
+}
+
+//Handler: Edit a wiki page
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
 }
 
 func main() {
     http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    //http.HandleFunc("/save/", saveHandler)
     http.ListenAndServe(":8080", nil)
 }
-
